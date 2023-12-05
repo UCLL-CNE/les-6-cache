98adc4e9b9c4b4c9baeb9983a8fa65146d7c5b87
artusvranken <artusvranken@gmail.com>
2023-12-05 17:27:12 +0100

    Redis showing weird behaviour

--- repository/redis-link-cache.ts
+++ repository/redis-link-cache.ts
@@ -1,17 +1,19 @@
 import { createClient } from "redis";
-import { Link } from "../domain/link";
 
 interface RedisProxyInterface {
-    set: (key: string, value: string) => Promise<string>,
+    set: (key: string, value: string, options?: {}) => Promise<string>,
     get: (key: string) => Promise<string>,
-    quit: () => Promise<string>
+    quit: () => Promise<string>,
+    isOpen: boolean,
+    isReady: boolean,
+    connect: () => Promise<unknown>
 }
 
 // Environment variables for cache
 export class LinkCache {
     private static instance: LinkCache;
 
-    private readonly cacheClient;
+    private readonly cacheClient: RedisProxyInterface;
 
     private constructor(redisClient: RedisProxyInterface) {
         if (!redisClient) {
@@ -20,26 +22,36 @@ export class LinkCache {
         this.cacheClient = redisClient;
     }
 
+    private static async createClient() {
+        const cacheHostName = process.env.REDIS_HOST_NAME;
+        const cachePassword = process.env.REDIS_ACCESS_KEY;
+        const cachePort = process.env.REDIS_PORT || "6380";
+
+        if (!cacheHostName) throw Error("REDIS_HOST_NAME is empty")
+        if (!cachePassword) throw Error("REDIS_ACCESS_KEY is empty")
+
+        const cacheConnection = createClient({
+            url: `rediss://${cacheHostName}:${cachePort}`,
+            password: cachePassword
+        });
+
+        await cacheConnection.connect();
+        return cacheConnection;
+    }
+
     static async getInstance() {
         if (!this.instance) {
-            const cacheHostName = process.env.REDIS_HOST_NAME;
-            const cachePassword = process.env.REDIS_ACCESS_KEY;
-            const cachePort = process.env.REDIS_PORT || "6380";
-
-            if (!cacheHostName) throw Error("REDIS_HOST_NAME is empty")
-            if (!cachePassword) throw Error("REDIS_ACCESS_KEY is empty")
-
-            const cacheConnection = createClient({
-                url: `rediss://${cacheHostName}:${cachePort}`,
-                password: cachePassword
-            });
-
-            await cacheConnection.connect();
-            cacheConnection.get("test");
-
+            const cacheConnection = await this.createClient();
             this.instance = new LinkCache(cacheConnection);
-
-
+        } else {
+            if (!this.instance.cacheClient.isOpen || !this.instance.cacheClient.isReady) {
+                try {
+                    await this.instance.cacheClient.connect();
+                } catch (error) {
+                    console.error(error);
+                    this.instance = new LinkCache(await this.createClient());
+                }
+            }
         }
         return this.instance;
     }
@@ -48,7 +60,7 @@ export class LinkCache {
         await this.cacheClient.quit();
     }
 
-    async setLinkMapping(link: string, mapping :string) {
+    async setLinkMapping(link: string, mapping: string) {
         await this.cacheClient.set(mapping, link, { EX: 600 });
     }
 
